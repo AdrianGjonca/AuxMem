d414e3d4578c111221498fc7dcbcd467c56ae7fb
Adrian Gjonca <gjoncaadrian@gmail.com>
2025-08-18 22:52:29 +0100

    Multi-byte r/w (untested)

--- HEADER/auxmem.h
+++ HEADER/auxmem.h
@@ -11,6 +11,12 @@ typedef enum {
 extern AM_error_t AM_error_g;
 
 extern void AM_init(const char * file, size_t size);
+
 extern void AM_setbyte(AM_addr_t address, uint8_t byte_value);
 extern uint8_t AM_getbyte(AM_addr_t address);
+
+extern void AM_setbytes(AM_addr_t address, uint8_t * byte_array, size_t size);
+extern void AM_getbytes(AM_addr_t address, uint8_t * byte_array, size_t size);
+extern uint8_t * AM_getbytes_heap(AM_addr_t address, size_t size);
+
 extern void AM_quit();
--- SOURCE/auxmem.c
+++ SOURCE/auxmem.c
@@ -84,6 +84,41 @@ uint8_t AM_getbyte(AM_addr_t address) {
 	return fgetc(swap_g);
 }
 
+void AM_setbytes(AM_addr_t address, uint8_t * byte_array, size_t size) {
+	if(address + size - 1 >= size_g) {
+		AM_error_g = AM_FAILURE;
+		return;
+	}else {
+		AM_error_g = AM_SUCCESS;
+	}
+	for(size_t i = 0; i < size; i++) {
+		AM_setbyte(address + i, byte_array[i]);
+	}
+}
+
+void AM_getbytes(AM_addr_t address, uint8_t * byte_array, size_t size) {
+	if(address + size - 1 >= size_g) {
+		AM_error_g = AM_FAILURE;
+		return;
+	}else {
+		AM_error_g = AM_SUCCESS;
+	}
+	for(size_t i = 0; i < size; i++) {
+		byte_array[i] = AM_getbyte(address + i);
+	}
+}
+
+uint8_t * AM_getbytes_heap(AM_addr_t address, size_t size) {
+	uint8_t * byte_array = malloc(size * sizeof(uint8_t));
+	if(byte_array == NULL) {
+		AM_error_g = AM_FAILURE;
+		return NULL;
+	}
+	//AM_error_g carried on from call below
+	AM_getbytes(address, byte_array, size); 
+	return byte_array;
+}
+
 void AM_quit() {
 	free(file_g);
 	fclose(swap_g);
